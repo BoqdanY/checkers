e287d591e48b2473cf7d6c20a712fa6340ea6092
thebladehit <boqdanyarmolka@gmail.com>
2023-01-29 14:30:34 +0200

    Checkers can do move

--- src/script.js
+++ src/script.js
@@ -1,7 +1,15 @@
 'use strict';
 class Area {
+    static area = [];
+
+    static clearArea() {
+        for (const row of Area.area) {
+            for (const col of row) {
+                col.classList.remove('available');
+            }
+        }
+    }
     constructor() {
-        this.array = [];
         for (let i = 0; i < 8; i++) {
             const arr = [];
             for (let j = 0; j < 8; j++) {
@@ -15,12 +23,12 @@ class Area {
                 }
                 arr.push(one);
             }
-            this.array.push(arr);
+            Area.area.push(arr);
         }
     }
     draw() {
         const baza = document.getElementById('baza');
-        for (const cols of this.array) {
+        for (const cols of Area.area) {
             const row = document.createElement('div');
             row.id = 'row';
             for(const col of cols) {
@@ -32,20 +40,66 @@ class Area {
 }
 
 class Checker {
-    constructor(color) {
+    static #checkers = [];
+    constructor(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.available = [];
         this.body = document.createElement('div');
-        if (color === 'green') {
-            this.body.classList.add('chest');
-            this.body.classList.add('chestGreen');
-        } else {
-            this.body.classList.add('chest');
-            this.body.classList.add('chestRed');
+        this.body.addEventListener('click', () => { // here must be something
+            this.addActiveStyle();
+            this.showAvailable();
+        });
+        this.body.classList.add('chest');
+        this.body.classList.add(color);
+        Checker.#checkers.push(this.body);
+    }
+
+    showAvailable() {
+        Area.clearArea();
+        this.available = this.getAvailable();
+        for (const elem of this.available) {
+            elem.classList.add('available');
+            elem.onclick = () => this.move(elem);
         }
+    }
+
+    move(elem) {
+        console.log(elem); // console
+        elem.append(this.body);
+        this.body.classList.remove('active');
+        [this.y, this.x] = elem.id.split(' ').map(x => Number(x));
+        Area.clearArea();
+        console.log(elem.id.split(' '));
+        for (const elem of this.available) {
+            elem.onclick = '';
+        }
+    }
+
+    getAvailable() {
+        const availableMoves = [];
+        const firstPlayerMoves = [`${this.y + 1} ${this.x + 1}`, `${this.y + 1} ${this.x - 1}`];
+        const secondPlayersMove = [`${this.y - 1} ${this.x - 1}`, `${this.y - 1} ${this.x + 1}`];
+        const moves = this.color === 'firstPlayer' ? firstPlayerMoves : secondPlayersMove;
+        for (const move of moves) {
+            const elem = document.getElementById(move);
+            if (elem === null) continue;
+            if (!elem.innerHTML) availableMoves.push(elem);
+        }
+        return availableMoves;
+    }
+
+    addActiveStyle() {
+        for (const elem of Checker.#checkers) {
+            elem.classList.remove('active');
+        }
+        this.body.classList.add('active');
     }
     static drawOnePlayer(x, y, color, start) {
         for (let i = 0; i < 12; i++) {
             const col = document.getElementById(`${y} ${x + start}`);
-            const checker = new Checker(color);
+            const checker = new Checker(x + start, y, color);
             col.append(checker.body);
             x += 2;
             if (x > 7) {
@@ -56,8 +110,8 @@ class Checker {
         }
     }
     static draw() {
-        Checker.drawOnePlayer(0, 0, 'green', 1);
-        Checker.drawOnePlayer(0, 5, 'chestRed', 0);
+        Checker.drawOnePlayer(0, 0, 'firstPlayer', 1);
+        Checker.drawOnePlayer(0, 5, 'secondPlayer', 0);
     }
 }
 
